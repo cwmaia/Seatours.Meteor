ec0060d4eac7fa9789929c5998fbeb5093ce49c9
jarbaspsf <jarbas.silva.f@gmail.com>
2013-10-25 17:12:55 -0300

    calc of book complete

--- client/views/book/book_operator.js
+++ client/views/book/book_operator.js
@@ -30,14 +30,7 @@ Template.productPrices.events({
 	"change input" : function(event){
 		var totalParcial = event.currentTarget.value * this.unit;
 		$('#'+this.price).val(totalParcial);
-
-		var total = 0;
-		$('.calcTotal').filter(function(){
-			if($(this).val() != "")
-			total += parseInt($(this).val());
-		})
-
-		$('#totalISK').val(total);
+		calcTotal();
 	}
 })
 
@@ -58,19 +51,78 @@ Template.bookingVehicles.rendered = function(){
 Template.bookingVehicles.events({
 	'change #listvehicle' : function(event){
 		var id = event.target.selectedOptions[0].id;
-		var category = Vehicles.findOne({_id: id}).category;
-		$("#categories option").filter(function(){
-			return $(this).text() == category.category;
-		}).attr('selected', true);
 
-		$("#size option:first").text(category.size+"m").val(category.size).attr("selected", true);
-		$("#categories").attr("disabled", true);
-		$("#size").attr("disabled", true);
-	},
+		if(id != ""){
+			var category = Vehicles.findOne({_id: id}).category;
+			$("#categories option").filter(function(){
+				return $(this).text() == category.category;
+			}).attr('selected', true);
 
-	'change #categories' : function(){
-		
+			$("#size option:first").text(category.size+"m").attr("selected", true);
+			$("#size").val(category.size);
+			
+			$("#categories").attr("disabled", true);
+			$("#size").attr("disabled", true);
+
+			$("#baseVehicle").val(category.basePrice);
+
+			var totalVehiclePrice = category.basePrice;
+			if(category.size > 10){
+				var mult = category.size - 10;
+				totalVehiclePrice += mult * 1625;
+			}
+
+			$("#totalVehicle").val(totalVehiclePrice);
+			calcTotal();
+		}else{
+			$("#categories").removeAttr("disabled");
+			$("#size").removeAttr("disabled");
+			$("#categories option:first").attr("selected", true);
+			$("#size option:first").text("").attr("selected", true);
+			$("#size option:first").attr("selected", true);
+			$('#vehiclesField input[type=text]').val('');
+			calcTotal();
+		}
 	}
 })
 
+Template.categoryVehicleBook.helpers({
+	categories : function(){
+		return VehiclesCategory.find();
+	}
+});
+
+Template.categoryVehicleBook.sizes = function() {
+	return Session.get('categoryId') ? VehiclesCategory.findOne({_id: Session.get('categoryId')}).size : [];
+}
+
+Template.categoryVehicleBook.events({
+	'change #categories' : function(event){
+		var id = event.target.selectedOptions[0].id;
+		var category = VehiclesCategory.findOne({_id: id});
+		$("#baseVehicle").val(category.basePrice);
+		Session.set('categoryId', id);
+	},
+
+	'change #size' : function(event){
+		var base = parseInt($("#baseVehicle").val());
+		var value = event.target.selectedOptions[0].value;
+		if(value > 10){
+			var mult = value - 10;
+			base += mult * 1625;
+		}
+		$("#totalVehicle").val(base);
+		calcTotal();
+	}
+})
+
+calcTotal = function(){
+		var total = 0;
+		$('.calcTotal').filter(function(){
+			if($(this).val() != "")
+			total += parseInt($(this).val());
+		})
+
+		$('#totalISK').val(total);
+}
 
