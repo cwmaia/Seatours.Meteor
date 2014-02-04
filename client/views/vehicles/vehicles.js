57fde265232179cfd1aa4092f8b9bdc8cef528de
jarbaspsf <jarbas.silva.f@gmail.com>
2014-02-04 11:14:01 -0300

    edit prices category vehicles

--- client/views/vehicles/vehicles.js
+++ client/views/vehicles/vehicles.js
@@ -13,17 +13,55 @@ Template.categoryVehicle.sizes = function() {
 Template.categoryVehicle.rendered = function(){
 	$(".formattedAsMoney").maskMoney({thousands:'.', allowNegative:'true', precision:'0'});
 	$(".formattedAsMoney").maskMoney('mask');
+	$("#editPricesCategory").hide();
 }
 
-Template.categoryVehicle.events({
-	'change #categories' : function(event){
-		var id = event.target.selectedOptions[0].id;
-		Session.set('categoryId', id);
+Template.vehiclesCategoryEdit.events({
+	'click .editPriceCategory' : function(event){
+		var a = event.currentTarget;
+
+		category = VehiclesCategory.findOne(a.rel);
+
+		$('input[name="basePriceCategory"]').val(category.basePrice);
+		$('input[name="stepCategory"]').val(category.step);
+		
+		if(category.onReduce){
+			$('input[name="stepOnReduce"]').attr('checked', true);
+		}
+
+		Session.set('editCategoryId', a.rel);
+		$("#editPricesCategory").show();
+	},
+
+	'click .close' : function(){
+		$("#editPricesCategory").hide();
+	},
+
+	'submit #formEditCategory' : function(event){
+		event.preventDefault();
+		var form = event.target;
+
+		VehiclesCategory.update(Session.get('editCategoryId'), {$set : {
+			basePrice : form.basePriceCategory.value,
+			step : form.stepCategory.value,
+			onReduce : $('input[name="stepOnReduce"]').is(":checked")
+		}});
+
+		$("#editPricesCategory").hide();
+		throwSuccess("Category Edited!");
+
 	}
+
+
 })
 
 
 Template.preRegisterVehicles.events({
+	'change #categories' : function(event){
+		var id = event.target.selectedOptions[0].id;
+		Session.set('categoryId', id);
+	},
+
 	'submit form' : function(event) {
 		event.preventDefault();
 		
